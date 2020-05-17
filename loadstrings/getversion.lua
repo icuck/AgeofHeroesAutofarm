caeef22f72817e393488cb5ec4d2dc4ba3eaad32
Larry L <tazerzx@yahoo.com>
2020-05-17 13:21:01 -0400

    Update getversion.lua

--- loadstrings/getversion.lua
+++ loadstrings/getversion.lua
@@ -1,2 +1,8 @@
 scriptversion = 0.5
 scriptlock = false
+
+
+local plr = game:GetService("Players").LocalPlayer
+if scriptlock = true then
+  plr:Kick("Script is locked for development purposes");
+end
